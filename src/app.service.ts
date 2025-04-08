1f3ca30848b143a6ebeccddac5198a72e2d90693
Mathis <mathis.laurent@epitech.eu>
2025-04-08 16:04:02 +0200

    feat: :sparkles: trigger lint fail and add annotations

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    const unusedVariable = 'This will cause a lint error';
     return 'Hello World!';
   }
 }
